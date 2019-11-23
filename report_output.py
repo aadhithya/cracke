a88812a9545b2cd8852cf46372259fea34259dfd
AbinavRavi <abinav.ravi@tum.de>
2019-11-23 16:51:20 +0100

    added script for calculations

--- report_output.py
+++ report_output.py
@@ -1,9 +1,33 @@
 import math
 import pandas as pd
 
-def report(length,breadth,time,material,load):
+def report_crack(length,breadth,time,material,max_load,min_load):
     crack_length = math.sqrt(length**2+breadth**2)
     file = pd.read_csv('./material.csv')
-    C_constant = file['C']
+    C_constant = file[(file['material']== material)&(file['time']==time)]['C']
+    m =  file[(file['material']== material)&(file['time']==time)]['m']
+    N = file[(file['material']== material)&(file['time']==time)]['cycles']
+    cycles = crack_length/(C*((max_load - min_load)*(10**5))**m)
+    if(cycles >  N):
+        num_cycles = 'Usage ' 
+    else:
+        num_cycles = 'manufacturing defect'
+    output = 'cracked'
+    return crack_length,output,num_cycles 
+
+def report_no_crack(length,breadth,time,material,max_load,min_load):
+    if(length == 0 and breadth == 0):
+        output = 'Not cracked'
+    file = pd.read_csv('./material.csv')
+    C_constant = file[(file['material']== material)&(file['time']==time)]['C']
+    m =  file[(file['material']== material)&(file['time']==time)]['m']
+    N = file[(file['material']== material)&(file['time']==time)]['cycles']
+    cycles = crack_length/(C*((max_load - min_load)*(10**5))**m)
+    if(cycles >  N):
+        num_cycles = 'perform maintanence' 
+    else:
+        num_cycles = 'The sample is okay'
+
+    return crack_length,output,num_cycles 
 
 
