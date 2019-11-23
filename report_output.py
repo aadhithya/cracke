f42ce6e36a542bcd9640e8e6bac2d778969d0354
Aadhithya Sankar <a.sankar@tum.de>
2019-11-23 21:53:08 +0100

    clean up, length, breadth None to 0

--- report_output.py
+++ report_output.py
@@ -8,7 +8,7 @@ def report_crack(length,breadth,time,material,max_load,min_load):
     N = file[(file['material']== material)&(file['time']==time)]['cycles'].values
     # print(N)
     
-    if(length == None and breadth== None):
+    if(length == 0 and breadth== 0):
         output = 'Not cracked'
         crack_length = 0
         num_cycles = 'No problem with the sample'
@@ -24,20 +24,20 @@ def report_crack(length,breadth,time,material,max_load,min_load):
     # print(crack_length,output,num_cycles)
     return crack_length,output,num_cycles 
 
-def report_no_crack(length,breadth,time,material,max_load,min_load):
-    if(length == None and breadth == None):
-        output = 'Not cracked'
-    file = pd.read_csv('./material.csv')
-    C_constant = file[(file['material']== material)&(file['time']==time)]['C']
-    m =  file[(file['material']== material)&(file['time']==time)]['m']
-    N = file[(file['material']== material)&(file['time']==time)]['cycles']
-    cycles = crack_length/(C*((max_load - min_load)*(10**5))**m)
-    if(cycles >  N):
-        num_cycles = 'perform maintanence' 
-    else:
-        num_cycles = 'The sample is okay'
+# def report_no_crack(length,breadth,time,material,max_load,min_load):
+#     if(length == None and breadth == None):
+#         output = 'Not cracked'
+#     file = pd.read_csv('./material.csv')
+#     C_constant = file[(file['material']== material)&(file['time']==time)]['C']
+#     m =  file[(file['material']== material)&(file['time']==time)]['m']
+#     N = file[(file['material']== material)&(file['time']==time)]['cycles']
+#     cycles = crack_length/(C*((max_load - min_load)*(10**5))**m)
+#     if(cycles >  N):
+#         num_cycles = 'perform maintanence' 
+#     else:
+#         num_cycles = 'The sample is okay'
 
-    return crack_length,output,num_cycles 
+#     return crack_length,output,num_cycles 
 
 
-report_crack(None,None,2,'cast iron',1000,200)
+# report_crack(None,None,2,'cast iron',1000,200)
